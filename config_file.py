ad8b38b92c74e07ee4f1158dab809737d21220a2
shreyashnadage <shreyashnadage@gmail.com>
2025-04-13 21:42:05 +0530

    fixed absolute path issue

--- config_file.py
+++ config_file.py
@@ -1,8 +1,8 @@
-f_path_in = 'D:\\Project_H\\Examples\\Example_1\\Inputnew'
-f_path_out = 'D:\\Project_H\\Examples\\Example_1\\Output'
+f_path_in = '.\Examples\\Example_1\\Inputnew'
+f_path_out = '.\Examples\\Example_1\\Output'
 
-f_path_trade_docs = 'D:\\Project_H\\Docs\\UserGuide\\tradedata'
-f_path_trade_samples = 'D:\\Project_H\\Examples\\Sample_Trades'
+f_path_trade_docs = '.\Docs\\UserGuide\\tradedata'
+f_path_trade_samples = '.\Examples\\Sample_Trades'
 
 
 class FileHandlingUtil:
